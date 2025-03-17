1406c5779f56ca1833f6cd3536994adc4a560e73
ymarting <ys.martin@upm.es>
2025-03-17 16:51:58 +0100

    Commit inicial con el proyecto de Spring Boot

---
+++ src/main/java/es/upm/dit/isst/tfgapi/TfgapiApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.tfgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/dit/isst/tfgapi/TfgapiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.tfgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
